654556fd89f3247ac08c761e6741977945f89702
Glitch (hibiki-verniy) <none>
2019-03-18 11:02:00 +0000

    🏤🤘 Checkpoint ./commands/info.js:1356156/237

--- commands/info.js
+++ commands/info.js
@@ -1,7 +1,7 @@
 exports.run = (client, message) => {
     const embed = new client.vembed();
     embed.setTitle(`🔒  Roles List [${message.guild.roles.size}]`);
-    embed.setDescription(`${message.guild.roles.map(roles => roles).join(' \n ')}`, true)
+    embed.setDescription(`${message.guild.roles.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(roles => roles).join(' \n ')}`, true);
     embed.setFooter(`Jumlah member : ${client.users.size}`)
     message.channel.send(embed);
 

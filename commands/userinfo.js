c1e86f5639a8d8d7a1fa9d695ec33c2a96e0b6b9
Glitch (hibiki-verniy) <none>
2019-03-18 23:52:57 +0000

    🚲🙀 Checkpoint ./commands/userinfo.js:1356156/687

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,16 +1,1 @@
-exports.run = async (client, message, args) => {
-    const moment = require("moment");
-
-    var member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(member === undefined || member === null){
-        const self = message.author.id
-        member = message.guild.member(self);
-    }
-    const joinServer = moment(member.joinedAt).format('llll');
-    const embed = new client.vembed();
-    embed.setTitle(`${member.user.username}'s Profile info.`)
-    embed.addField('Joined at:', `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-    message.channel.send(embed);
-    console.log(member.joinedAt);
-    
-}
+Q

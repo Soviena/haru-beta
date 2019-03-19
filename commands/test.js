d82f4e8e56548a4cd0e9bb4201ae35acfd15bff1
Glitch (hibiki-verniy) <none>
2019-03-19 03:33:02 +0000

    🏛🐇 Checkpoint ./commands/test.js:1356156/540

--- commands/test.js
+++ commands/test.js
@@ -1,15 +1,4 @@
 exports.run = async (client, message, args) => {
-    const moment = require("moment");
-
-    var member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(member === undefined || member === null){
-        const self = message.author.id
-        member = message.guild.member(self);
-    }
-    const joinServer = moment(member.joinedAt).format('llll');
-    const embed = new client.vembed();
-    
-    var size = member.guild.members.filter(member => !member.user.bot).size;
-    console.log(size);
+  const db = require('quick.db');
     
 }

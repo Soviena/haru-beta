3cf0e557aa9ae5bd984900d4a20b9848536c042d
Glitch (hibiki-verniy) <none>
2019-03-19 14:04:01 +0000

    🔋🏔 Checkpoint ./commands/kill.js:1356156/409 ./data/kill.js:1356156/1544

--- commands/kill.js
+++ commands/kill.js
@@ -1,11 +1,15 @@
 exports.run = (client, message, args) => {
+    var member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(member === undefined || member === null){
+    const id = message.author.id
+    member = message.guild.member(id);
+    }
     
-    
-    const embed = new client.vembed();
-    var kill = require('../data/pout'),
+    var kill = require('../data/kill'),
     killkeyarr = Object.keys(kill),
     kindex = Math.floor(Math.random() * killkeyarr.length),
     bunuh = kill[killkeyarr[kindex]];
+    message.channel.send(`${member.user.username} ${bunuh} `);
 
     
 }

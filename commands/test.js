ed651e9926481b2d8213bfb705fdb51f29126cf9
Glitch (hibiki-verniy) <none>
2019-03-20 04:19:37 +0000

    🎟🎺 Checkpoint ./commands/test.js:1356156/165

--- commands/test.js
+++ commands/test.js
@@ -1,10 +1,13 @@
 exports.run = (client, message, args) => {
   
+  if(message.author.id !== client.config.ownerID) {
+  message.reply("Kamu Bukan Master Ku!");
+  return;
+  }
+  
   //var ch = args[0].slice(2, -1);
   //args.shift();
   var txt = args.join(' ');
   //client.channels.get('556795240682618881').send(txt);
-  client.guilds.get('556788141202210816').channels.get('556795240682618881').send(txt)
-  console.log(args);
-  
+  client.guilds.get('556788141202210816').channels.get('556795240682618881').send(txt)  
 }

157d06cdac4604d5c2703c3d69473b0c92499c9b
Glitch (hibiki-verniy) <none>
2019-03-20 03:47:32 +0000

    😅💎 Checkpoint ./commands/test.js:1356156/999

--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,5 @@
 exports.run = (client, message, args) => {
-  var ch;
   
-	function regex(args) {
-  
-  if (args.startsWith('<#') && args.endsWith('>')) {
-		ch = args.slice(2, -1);
-
-		console.log(ch);
-	}
-  
-  }
+  var ch = args.slice(2, -1);
+  console.log(ch);
 }

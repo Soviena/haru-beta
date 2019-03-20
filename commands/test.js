fbb20d9501db02fc5de79fa2159d6940a4c24065
Glitch (hibiki-verniy) <none>
2019-03-20 04:09:25 +0000

    ⛑⛴ Checkpoint ./commands/test.js:1356156/181

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,10 @@
 exports.run = (client, message, args) => {
   
-  var ch = args[0].slice(2, -1);
-  args.shift();
+  //var ch = args[0].slice(2, -1);
+  //args.shift();
   var txt = args.join(' ');
-  client.channels.get(ch).send(txt);
+  //client.channels.get('556795240682618881').send(txt);
+  client.guilds.get('556788141202210816').channels.get('556795240682618881').send(txt)
   console.log(args);
   
 }

97d2fd5cd16b0f49642df9694770add80c38f0f8
Glitch (hibiki-verniy) <none>
2019-03-20 01:24:37 +0000

    🍤🐞 Checkpoint ./commands/test.js:1356156/51

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     
-    client.channels.get('551536245987672100').send('dikirim dari channel bot')
+    client.channels.get('557536245987672100').send('dikirim dari channel bot');
     
   
 

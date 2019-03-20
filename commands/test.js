eb039381b1e1fa18fe45b7cf60ed827eb67889de
Glitch (hibiki-verniy) <none>
2019-03-20 02:41:36 +0000

    🐵🚒 Checkpoint ./commands/test.js:1356156/191

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
-  
+    var ch = message.guild.channel(message.guild.channel.get(args[0]));
     console.log(message.channel.id);
-    console.log(args);
+    console.log(ch);
   
 
 }

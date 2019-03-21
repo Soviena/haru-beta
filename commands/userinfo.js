2f934255c2dea3660a86dabb3e6eaee40679c700
Glitch (hibiki-verniy) <none>
2019-03-21 00:34:26 +0000

    📯⛷ Checkpoint ./commands/userinfo.js:1356156/715

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,5 +1,6 @@
 exports.run = async (client, message, args) => {
     const moment = require("moment");
+    moment.locale('id');
 
     var member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(member === undefined || member === null){
@@ -10,16 +11,17 @@ exports.run = async (client, message, args) => {
     // get day elapsed
     var oneDay = 24*60*60*1000;
     var time = new Date(moment.utc(member.joinedAt).format('YYYY,MM,DD'));
+    var joint = new Date(moment.utc(member.user.createdAt).format('YYYY,MM,DD'));
     var a = new Date(moment.utc(member.lastMessage.createdTimestamp).format('YYYY,MM,DD'));
     var diffDays = Math.round(Math.abs((a.getTime() - time.getTime())/(oneDay)));
+    var diffDaysj = Math.round(Math.abs((a.getTime() - joint.getTime())/(oneDay)));
   
     const joinServer = moment(member.joinedAt).format('llll');
     const embed = new client.vembed();
     embed.setAuthor(`${member.user.username}#${member.user.discriminator} ~ ${message.guild.members.get(member.user.id).nickname}`);
     embed.setThumbnail(member.user.avatarURL);
-    embed.addField('Membuat akun discord pad : ');
-    embed.addField('Bergabung dengan server pada:', `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')} \n (${diffDays}) hari yang lalu`, true);
+    embed.addField('Membuat akun discord pada : ', `${moment.utc(member.user.createdAt).format('dddd, Do MMMM YYYY')} \n (${diffDaysj}) hari yang lalu`, true);
+    embed.addField('Bergabung dengan server pada:', `${moment.utc(member.joinedAt).format('dddd,Do MMMM YYYY')} \n (${diffDays}) hari yang lalu`, true);
     message.channel.send(embed);
-    console.log(member.user.createdAt);
     
 }

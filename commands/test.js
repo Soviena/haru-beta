ecc07494a1a21187ae0d4a73037bc3041b33e50b
Glitch (hibiki-verniy) <none>
2019-03-21 01:18:19 +0000

    🐜📖 Checkpoint ./commands/lbinvite.js:1356156/26 ./commands/test.js:1356156/955

--- commands/test.js
+++ commands/test.js
@@ -4,34 +4,5 @@ exports.run = async (client, message, args) => {
   return;
   }
     
-  const arraySort = require('array-sort');
-
-  let invites = await message.guild.fetchInvites().catch(error => { 
-    return message.channel.send('Aku gk punya wewenangs!');
-    }) 
-
-  invites = invites.array();
-
-  arraySort(invites, 'uses', { reverse: true }); 
-
-  let possibleinvites = [];
-  let index = 0;
-  invites.forEach(function(invites) {
-    possibleinvites.push(`**${++index}**. 🔸 **${invites.inviter.tag}** 》 \`${invites.uses}\` **invites**`)
-    })
-
-  const embed = new client.vembed()
-    .setTitle(`🏆 INVITE LEADERBOARD 🏆`)
-    .setColor('RANDOM')
-    .setDescription(`${possibleinvites.join('\n')}`)
-    .setTimestamp();
-    message.channel.send(embed);
-
-  //var server = 
-  
-  //var ch = args[0].slice(2, -1);
-  //args.shift();
-  //var txt = args.join(' ');
-  //client.channels.get('556795240682618881').send(txt);
-  //client.guilds.get('556788141202210816').channels.get('556795240682618881').send(txt)  
+  console.log(client.)
 }

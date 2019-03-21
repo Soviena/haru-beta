a7fd6a0f89779d5b4a8451d935c3233d0706a5da
Glitch (hibiki-verniy) <none>
2019-03-21 02:56:21 +0000

    🎰✂️ Checkpoint ./commands/glist.js:1356156/340 ./commands/infobot.js:1356156/3

--- commands/infobot.js
+++ commands/infobot.js
@@ -4,7 +4,7 @@ exports.run = (client, message) => {
   embed.setTitle('Info Bot Hibiki Verniy');
   embed.setDescription('Bot yang di buat oleh Verniy#2201');
   embed.addField('Di hosting di :' , 'GLITCH');
-  embed.addField('Versi Bot : ', 'v1.10');
+  embed.addField('Versi Bot : ', 'v2.0');
   embed.addField('Aktif di : ' , `${client.guilds.size} server`);
   embed.addField('Melayani : ', `${client.users.size} member`);
   embed.setColor(0x5effd9);

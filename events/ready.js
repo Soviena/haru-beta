2e22897156eea4d25596a55fb4c1bd6ea73c3a75
Glitch (hibiki-verniy) <none>
2019-03-25 05:30:16 +0000

    💪🏆 Checkpoint ./events/ready.js:1356156/563 ./main.js:1356156/51

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 module.exports = (client) => {
   const moment = require("moment");
+  const db = client.sql;
   const dba = client.serv;
   console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
   
@@ -11,6 +12,16 @@ module.exports = (client) => {
     let random = status[Math.floor(Math.random() * status.length)]
     client.user.setActivity(random , { type: 'STREAMING', url: `https://www.twitch.tv/vizsher`});
     
+  }
+  
+    const table = db.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'Rp';").get();
+    if (!table['count(*)']) {
+    // If the table isn't there, create it and setup the database correctly.
+    db.prepare("CREATE TABLE Rp (id TEXT PRIMARY KEY, user TEXT, guild TEXT, Respect INTEGER);").run();
+    // Ensure that the "id" row is always unique and indexed.
+    db.prepare("CREATE UNIQUE INDEX idx_scores_id ON Rp (id);").run();
+    db.pragma("synchronous = 1");
+    db.pragma("journal_mode = wal");
   }
   
     const dbo = dba.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'data';").get();

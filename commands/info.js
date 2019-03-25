2a1d553e72862dc6e34684d29c944b08482bcaf0
Glitch (hibiki-verniy) <none>
2019-03-25 23:48:47 +0000

    🏙🐺 Checkpoint ./commands/info.js:1356156/105

--- commands/info.js
+++ commands/info.js
@@ -1,11 +1,11 @@
 exports.run = (client, message) => {
-    const embed = new client.vembed();
-    embed.setTitle(`🔒  Roles List [${message.guild.roles.size}]`);
-    embed.setDescription(`${message.guild.roles.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(roles => roles).join(' \n ')}`, true);
-    embed.setFooter(`Jumlah member : ${message.guild.members.filter(member => !member.user.bot).size} dengan ${message.guild.channels.size} channel `);
-    embed.setColor(0xff38c0);
-    message.channel.send(embed);
-
+    const embed = new client.vembed()
+    .setAuthor(`message.guild.id, message.guild.iconURL`)
+    .setTitle(`🔒  Roles List [${message.guild.roles.size}]`)
+    .setDescription(`${message.guild.roles.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(roles => roles).join(' \n ')}`, true)
+    .setFooter(`Jumlah member : ${message.guild.members.filter(member => !member.user.bot).size} dengan ${message.guild.channels.size} channel `)
+    .setColor(0xff38c0)
+    .channel.send(embed);
 
 }
     

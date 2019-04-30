3d5399f4cb79300f6532fc727ba344287a008f0c
Glitch (hibiki-verniy) <none>
2019-04-30 04:03:28 +0000

    ⛈😄 Checkpoint ./commands/avatar.js:1356156/4

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,4 +1,4 @@
-exports.run = async (client, message, args) => {
+Tapiexports.run = async (client, message, args) => {
     var member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(member === undefined || member === null){
         const avatar = message.author.id

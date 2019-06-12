23f1335608aa1a8dd871998086a54b8e2c992fc7
Glitch (hibiki-verniy) <none>
2019-06-12 16:56:16 +0000

    🚇🚖 Checkpoint ./commands/test.js:1356156/2327

--- commands/test.js
+++ commands/test.js
@@ -1,26 +1,9 @@
-    
 exports.run = (client, message, args) => {
-    if(message.author.id !== client.config.ownerID) {
-    message.reply("Kamu Bukan Master Ku!");
-    return;
-    }
-    const sql = client.serv;
-    const list = sql.prepare("SELECT * FROM data DESC LIMIT 20;").all();
-     
-    var txt = args.join(' ');
-    
-    // Now shake it and show it! (as a nice embed, too!)
-    const embed = new client.vembed();
-    embed.setAuthor(message.guild.name, message.guild.iconURL)
-    .setDescription(`${txt}`)
-    .setColor('RANDOM')
-    .setFooter(`${message.author.username}#${message.author.discriminator} `, message.author.avatarURL)
-    .setTimestamp();
-    for(const data of list) {
-      if(data.id !== message.guild.id) {
-         console.log(client.guilds.get("'"+data.id+"'").name);
-      client.guilds.get("'"+data.id+"'").channels.get("'"+data.channel+"'").send(embed);
-      }  
-    }  
+const sql = client.serv;
+const list = sql.prepare("SELECT * FROM data DESC LIMIT 20;").all();
+const id =  297540702315741194;
+sql.prepare(`DELETE FROM data WHERE id = ${id}`).run();
+  message.channel.send("")
+  
   
 }

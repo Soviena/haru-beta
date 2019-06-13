8203c35f1da1766bb958479c265d4bda9367bd7a
Glitch (hibiki-verniy) <none>
2019-06-13 16:57:54 +0000

    🎑🍶 Checkpoint ./commands/test4.js:1356156/538

--- commands/test4.js
+++ commands/test4.js
@@ -2,12 +2,10 @@ exports.run = (client, message, args) => {
 const sql = client.serv; 
 
   
-  let data = client.getData.get(message.guild.id);
-  if (!data) { 
-    message.reply('Di server ini tidak ada integrasi Global Chat');
-  } else {
-    sql.prepare(`DELETE FROM data WHERE id = ${message.guild.id}`).run();
-    message.reply('Integrasi GLOBAL CHAT telah di hapus');  
-    }
+  let data = client.getData.get('255589775992553472');
+
+  sql.prepare(`DELETE FROM data WHERE id = 255589775992553472`).run();
+      message.reply('Integrasi GLOBAL CHAT telah di hapus');  
+
 }
 

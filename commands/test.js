de650e7bd87018b43d2f961ea8940d8013f7bd40
Glitch (hibiki-verniy) <none>
2019-06-13 04:44:34 +0000

    🎨🍖 Checkpoint ./commands/test.js:1356156/327

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
   
     const embed = new client.vembed();
     var id = args;
-    console.log(id);
+    console.log(client.guilds.get(id));
     var url = client.guilds.get(id).iconURL;
     embed.setAuthor(client.guilds.get(id).owner.user.username, client.guilds.get(id).owner.user.avatarURL)
     .setDescription(`${client.guilds.get(id).name}`)

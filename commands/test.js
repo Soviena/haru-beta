fb8c8f3bb02aa0c644b45d18e3c49e3e66d1e815
Glitch (hibiki-verniy) <none>
2019-06-13 02:21:22 +0000

    🌴🌺 Checkpoint ./commands/test.js:1356156/614

--- commands/test.js
+++ commands/test.js
@@ -1,4 +1,19 @@
 exports.run = (client, message, args) => { 
-  client.guilds
+  
+    const embed = new client.vembed();
+    embed.setAuthor(message.guild.name, message.guild.iconURL)
+    .setDescription('')
+    .setImage(url)
+    .setColor('RANDOM')
+    .setFooter(`${message.author.username}#${message.author.discriminator} `, message.author.avatarURL)
+    .setTimestamp();
+ 
+    for(const data of list) {
+      if(data.id !== message.guild.id) {
+        if(client.guilds.get(data.id) !== undefined){
+          client.guilds.get(data.id).channels.get(data.channel).send(embed);
+        } else {}
+      }  
+    }  
   
 }

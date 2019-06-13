fcc79ae47745d709a1046a168e367f1263f9d804
Glitch (hibiki-verniy) <none>
2019-06-13 01:50:46 +0000

    🏁🏣 Checkpoint ./commands/test2.js:1356156/460

--- commands/test2.js
+++ commands/test2.js
@@ -1,16 +1,10 @@
 exports.run = (client, message, args) => {  
   const db = client.serv;
     
-  var server = message.guild.id;
+  var server = '255589775992553472';
   
   let data = client.getData.get(server);
     if (!data) { 
-    if(args === null || args === undefined || args.length == 0) return message.reply('perlu nama channel cth : *gset #general');
-    var channel = args[0].slice(2, -1); 
-    if(isNaN(channel)){
-      message.reply("Format penulisan nama channel salah!");
-      return;
-    }
     data = {
       id: '255589775992553472',
       guild: '255589775992553472',
@@ -18,7 +12,6 @@ exports.run = (client, message, args) => {
       }
     client.setData.run(data);  
     message.channel.send("GLOBAL CHAT sudah di setting");
-    message.channel.send(`server : ${message.guild.name} ,di channel <#${channel}>`);
     
     } 
   

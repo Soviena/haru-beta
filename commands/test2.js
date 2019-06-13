4eaed0fa1ea8d7bcf016ca92a5b6167ed62cf49d
Glitch (hibiki-verniy) <none>
2019-06-13 16:35:57 +0000

    💴🐺 Checkpoint ./commands/test2.js:1356156/841

--- commands/test2.js
+++ commands/test2.js
@@ -1,10 +1,20 @@
 exports.run = (client, message, args) => { 
+  const sql = client.serv;
+const list = sql.prepare("SELECT * FROM data DESC LIMIT 20;").all();
   
         if(message.author.id !== client.config.ownerID) {
       message.reply("Kamu Bukan Master Ku!");
       return;
     }else {   
-  
-    message.channel.send(message.guild.iconURL);
+  const embed = new client.vembed();
+    embed.setTitle("DATA LIST")
+    .setAuthor("ALIANSI undefined")
+    .setDescription("SERVER YANG TERDAFTAR GLOBAL CHAT")
+    .setColor(0x00AE86);
+ 
+  for(const data of list) {
+    message.channel.send(`${data.id}`)
+  }
+  message.channel.send({embed});
     }
 }

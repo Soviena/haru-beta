13ddd27ef04452cfb0d8e3e9dab3198068300e8a
Glitch (hibiki-verniy) <none>
2019-06-13 04:55:34 +0000

    📮💴 Checkpoint ./commands/test.js:1356156/249

--- commands/test.js
+++ commands/test.js
@@ -2,10 +2,11 @@ exports.run = (client, message, args) => {
   
     const embed = new client.vembed();
     var id = args;
-    console.log(client.guilds.get(id));
-    var url = client.guilds.get(id).iconURL;
-    embed.setAuthor(client.guilds.get(id).owner.user.username, client.guilds.get(id).owner.user.avatarURL)
-    .setDescription(`${client.guilds.get(id).name}`)
+    console.log(id);
+    console.log(client.guilds.get(`${id}`).name);
+    var url = client.guilds.get(`${id}`).iconURL;
+    embed.setAuthor(client.guilds.get(`${id}`).owner.user.username, client.guilds.get(`${id}`).owner.user.avatarURL)
+    .setDescription(`${client.guilds.get(`${id}`).name}`)
     .setImage(url)
     .setColor('RANDOM');
     message.channel.send(embed);

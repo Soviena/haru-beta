8c07c56d7315eb48d7bcf59df974f1362990b537
Glitch (hibiki-verniy) <none>
2020-06-06 10:40:59 +0000

    👽👘 Checkpoint ./commands/info.js:1356156/14

--- commands/info.js
+++ commands/info.js
@@ -3,7 +3,7 @@ exports.run = (client, message) => {
     const split = description.match(/[\s\S]{1,2048}/g);
     for (let i = 0; i < split.length; i++) {
       let embed = new client.vembed()
-    .setAuthor(`${message.guild.id}, ${message.guild.iconURL}`)
+    .setAuthor(`${message.guild.name}`, `${message.guild.iconURL}`)
     .setTitle(`🔒  Roles List [${message.guild.roles.size}]`)
     .setDescription(split[i], true)
     .setFooter(`Jumlah member : ${message.guild.members.filter(member => !member.user.bot).size} dengan ${message.guild.channels.size} channel `)

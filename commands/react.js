23eab967979a7faf8a890bdeba40c2b0d08484d0
Glitch (hibiki-verniy) <none>
2020-06-07 09:22:11 +0000

    🎁👀 Checkpoint ./package.json:1356156/15601 ./commands/react.js:1356156/1

--- commands/react.js
+++ commands/react.js
@@ -1,6 +1,6 @@
 exports.run = (client, message) => {
     message.react("586723200281608193")
-    .then(() => message.react("586723106039529472"))
+    .then(() => message.react("586723106039529472""))
     .catch(() => console.error('One of the emojis failed to react.'));
     }
     

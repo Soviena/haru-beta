6bafb5d389e9ca93e50b381b8259b0804f77ca03
Glitch (hibiki-verniy) <none>
2020-06-07 14:38:06 +0000

    🙍👦 Checkpoint ./commands/react.js:1356156/5

--- commands/react.js
+++ commands/react.js
@@ -1,7 +1,7 @@
 exports.run = (client, message) => {
     const embed = new client.vembed();
 
-
+/*
     embed.setTitle(`Identity & Hobby`)
     .addField(':green_circle: : Calm Green',":green_square: : Lime Green")
     .addField(':yellow_circle: : Yelloww',":orange_circle: : Orangers")
@@ -19,7 +19,6 @@ exports.run = (client, message) => {
     .then(() => client.user.lastMessage.react('🔴'))    
     .catch(() => console.error('One of the emojis failed to react.'));
 
-/*
     embed.setTitle(`Identity & Hobby`)
     .addField(':computer: : Programmer',":camera: : Photographer")
     .addField(':pencil: : Editor',":straight_ruler: : Designer")

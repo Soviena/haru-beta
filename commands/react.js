bea6e34eed2b726af1664ee530dd56245e77d1ae
Glitch (hibiki-verniy) <none>
2020-06-07 11:07:17 +0000

    🙍🌋 Checkpoint ./commands/react.js:1356156/84

--- commands/react.js
+++ commands/react.js
@@ -1,6 +1,7 @@
 exports.run = (client, message) => {
-    message.react("586723200281608193")
-    .then(() => message.react("586723106039529472 "))
+    message.channel.send("Tekan emoji")
+    .then(() => message.react("586723200281608193"))
+    .then(() => message.react("586723106039529472"))
     .catch(() => console.error('One of the emojis failed to react.'));
     }
     

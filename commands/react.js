84cb140cbafc439a1702e7d7ba74c00433a8be61
Glitch (hibiki-verniy) <none>
2020-06-07 13:15:10 +0000

    🌾🏀 Checkpoint ./commands/react.js:1356156/1151

--- commands/react.js
+++ commands/react.js
@@ -1,16 +1,17 @@
 exports.run = (client, message) => {
     const embed = new client.vembed();
 
-    embed.setTitle(`SELF ROLE`)
-    .addField("Identity", ":flag_jp: : おたく")
-    .addField(':regional_indicator_r: : R++',":regional_indicator_r: : R++")
-    .addField(':regional_indicator_r: : R++',":regional_indicator_r: : R++")
-    .addField(':regional_indicator_r: : R++',":regional_indicator_r: : R++")
-    .addField(':regional_indicator_r: : R++',":regional_indicator_r: : R++")
-    .addField(':regional_indicator_r: : R++',":regional_indicator_r: : R++")
-    .addField(':regional_indicator_r: : R++',":regional_indicator_r: : R++")
-    .addField(':regional_indicator_r: : R++',":regional_indicator_r: : R++")
-    .addField(':regional_indicator_r: : R++',":regional_indicator_r: : R++")
+    embed.setTitle(`GAME ROLES`)
+    .addField(':regional_indicator_a: : Apex Legend',":regional_indicator_b: : Black Dessert")
+    .addField(':regional_indicator_c: : Brawlhalla',":regional_indicator_d: : The Forest")
+    .addField(':regional_indicator_e: : CSGO',":regional_indicator_f: : Destiny 2")
+    .addField(':regional_indicator_g: : Dota 2',":regional_indicator_h: : Fornite")
+    .addField(':regional_indicator_i: : GTA V',":regional_indicator_j: : Monster Hunter World")
+    .addField(':regional_indicator_k: : Minecraft',":regional_indicator_l: : Mobile Legends")
+    .addField(':regional_indicator_m: : OSU',":regional_indicator_n: : Overwatch")
+    .addField(':regional_indicator_o: : PUBG Mobile',":regional_indicator_p: : PUBG Pc")
+    .addField(':regional_indicator_q: : Rainbow Six',":regional_indicator_r: : Pokemon GO")
+    .addField(':regional_indicator_s: : Valorant',":regional_indicator_t: : Factorio")
     .setColor(0x00e4e8);
     message.channel.send(embed);
     /*

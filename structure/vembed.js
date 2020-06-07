de4a063cff5c1f6655f4f2e3a938f9c734bd044e
Glitch (hibiki-verniy) <none>
2020-06-07 10:20:20 +0000

    🍪🕶 Checkpoint ./structure/vembed.js:1356156/14

--- structure/vembed.js
+++ structure/vembed.js
@@ -1,4 +1,4 @@
-//const { RichEmbed } = require('discord.js')
+const { RichEmbed } = require('discord.js')
 
 /**
  * A RichEmbed with the default fields already filled
@@ -6,9 +6,9 @@
  * @param {User} [user] - The user that executed the command that resulted in this embed
  * @param {object} [data] - Data to set in the rich embed
  */
-//module.exports = class vembed extends RichEmbed {
-  //constructor (user, data = {}) {
-    //super(data)
-    //if (user) this.setFooter(user.tag)
- // }
-//}
+module.exports = class vembed extends RichEmbed {
+  constructor (user, data = {}) {
+    super(data)
+    if (user) this.setFooter(user.tag)
+  }
+}

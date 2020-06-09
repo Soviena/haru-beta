28cd0da02864d45bcf1ba1db3c2fad4f97d5c442
Glitch (hibiki-verniy) <none>
2020-06-09 09:48:26 +0800

    Synced with live version

---
+++ commands/gsend.js
@@ -0,0 +1,25 @@
+exports.run = (client, message, args) => {
+    if(message.author.id !== client.config.ownerID) {
+    message.reply("Kamu Bukan Master Ku!");
+    return;
+    }
+    const sql = client.serv;
+    const list = sql.prepare("SELECT * FROM data DESC LIMIT 20;").all();
+     
+    var txt = args.join(' ');
+    
+    // Now shake it and show it! (as a nice embed, too!)
+    const embed = new client.vembed();
+    embed.setAuthor(message.guild.name, message.guild.iconURL)
+    .setDescription(`${txt}`)
+    .setColor('RANDOM')
+    .setFooter(`${message.author.username}#${message.author.discriminator} `, message.author.avatarURL)
+    .setTimestamp();
+ 
+    for(const data of list) {
+      if(data.id !== message.guild.id) {
+      client.guilds.get(data.id).channels.get(data.channel).send(embed);
+      }  
+    }  
+  
+}
---
+++ commands/slist.js
@@ -0,0 +1,18 @@
+
+exports.run = (client, message, args) => {
+const sql = client.serv;
+const list = sql.prepare("SELECT * FROM data DESC LIMIT 20;").all();
+ 
+    // Now shake it and show it! (as a nice embed, too!)
+  const embed = new client.vembed();
+    embed.setTitle("DATA LIST")
+    .setAuthor(client.user.username, client.user.avatarURL)
+    .setDescription("SERVER YANG TERDAFTAR GLOBAL CHAT")
+    .setColor(0x00AE86);
+ 
+  for(const data of list) {
+    embed.addField(`${data.id}`, `--> ${data.channel}`);
+  }
+  message.channel.send({embed});
+  
+}
